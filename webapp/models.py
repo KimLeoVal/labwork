2d22edb906413aef3ea15f2b17e8cbdd82e17bf6
KimLeoVal <kimleo@mail.ru>
2022-07-03 23:09:01 +0600

    изменил 'remain' от IntegerField к PositiveIntegerField

--- webapp/models.py
+++ webapp/models.py
@@ -8,5 +8,5 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=2000, null=True, blank=True)
     category = models.TextField(max_length=30, choices=CHOICE, default=CHOICE[0][0])
-    remain = models.IntegerField(validators=[MinLengthValidator(0)])
+    remain = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
