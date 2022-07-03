eef4b916cc55f993803d789acec45fc021f961d3
KimLeoVal <kimleo@mail.ru>
2022-07-03 22:46:59 +0600

    создал модель Product, зарегил в админке, создал админа, создал и имигрировал миграцию

--- webapp/models.py
+++ webapp/models.py
@@ -1,9 +1,12 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
+
+CHOICE = [('other', 'разное'), ('bred', 'хлебо-булочные'), ('milk', 'молочка'), ('alco', 'алкоголь')]
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=2000, null=True,blank=True)
-    category = models.Choices()
-
-
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    category = models.TextField(max_length=30, choices=CHOICE, default=CHOICE[0][0])
+    remain = models.IntegerField(validators=[MinLengthValidator(0)])
+    price = models.DecimalField(max_digits=7, decimal_places=2)
